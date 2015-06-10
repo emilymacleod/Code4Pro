384c312415cd42202ce29f38123dd9a890faa0d0
emilymacleod <margaretemacleod@gmail.com>
2015-06-10 14:27:29 -0300

    Facebook auth
    Facebook auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,4 +16,7 @@ Devise.setup do |config|
 
   require 'omniauth-google-oauth2'
   config.omniauth :google_oauth2, '780428032143-e6as191vsktl2iel4ikg4ff699fb24la.apps.googleusercontent.com', 'YqjDw34OShhNrvkIWdWDKKZO', {access_type: "offline", approval_prompt: ""} 
+
+   require 'omniauth-facebook'
+   config.omniauth :facebook, '1673821026172238', 'fce8c8e1adb95a9d49f54553c915866d'
 end
