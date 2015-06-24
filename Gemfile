f80bacb34e5c2fb0d2658848c5cec58d902f49e8
emilymacleod <margaretemacleod@gmail.com>
2015-06-24 14:10:10 -0300

    drag and drop in dashboard
    drag and drop in dashboard

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem 'coderay', '~> 1.1.0'
 
 gem 'friendly_id', '~> 5.1.0'
 
+gem 'active_admin-sortable_tree', '~> 0.2.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: [:slugged, :finders]
-	
+
 	has_many :tasks
 
 	validates :name, presence: true, length: { maximum: 50 }
@@ -11,4 +11,7 @@ class Project < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  	def shortname 
+  		name.length > 25? name[0..25] + "..." : name
+  	end
 end
